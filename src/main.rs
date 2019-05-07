90bb5402032e5fe37bb4aafe8b9564d5e0908066
technetos <mrgould93@gmail.com>
2019-05-07 19:31:06 -0400

    implement battery meter

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::fs;
+use std::io::Read;
+use std::str::FromStr;
+
+const CHARGE_NOW: &'static str = "/sys/class/power_supply/BAT0/charge_now";
+const ENERGY_NOW: &'static str = "/sys/class/power_supply/BAT0/energy_now";
+const CHARGE_FULL: &'static str = "/sys/class/power_supply/BAT0/charge_full";
+const ENERGY_FULL: &'static str = "/sys/class/power_supply/BAT0/energy_full";
+
+#[derive(Debug)]
+enum Error {
+    Io(std::io::Error),
+    ParseInt(std::num::ParseIntError),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(e: std::num::ParseIntError) -> Self {
+        Error::ParseInt(e)
+    }
+}
+
+fn read_file(charge: &str, energy: &str) -> Result<u64, Error> {
+    let mut value = String::new();
+
+    let mut file = fs::File::open(charge);
+    if file.is_err() {
+        file = fs::File::open(energy);
+    }
+
+    let mut file = file?;
+    let _ = file.read_to_string(&mut value);
+    let num = u64::from_str(&value[..&value.len()-1])?;
+    Ok(num)
+}
+
+fn main() {
+    let now = read_file(CHARGE_NOW, ENERGY_NOW).unwrap();
+    let full = read_file(CHARGE_FULL, ENERGY_FULL).unwrap();
+
+    println!("{}%", now/(full/100));
+}
